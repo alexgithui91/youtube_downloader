017e9e4073d277a9841304c5c33376b84461013a
alexgithui91 <alexgithui91@gmail.com>
2022-01-14 15:11:23 +0300

    youtube downloader with progress bar

--- downloader.py
+++ downloader.py
@@ -1,15 +1,14 @@
 from pytube import YouTube
+from pytube.cli import on_progress
 
 DOWNLOAD_FOLDER = "C:\\Users\\Agithui\\Downloads"
 
 video_url = input("Please enter video url : ")
 
-video_obj = YouTube(video_url)
+video_obj = YouTube(video_url, on_progress_callback=on_progress)
 
 stream = video_obj.streams.get_highest_resolution()
 
 print("Downloading file...")
 
 stream.download(DOWNLOAD_FOLDER)
-
-print("Done...")
